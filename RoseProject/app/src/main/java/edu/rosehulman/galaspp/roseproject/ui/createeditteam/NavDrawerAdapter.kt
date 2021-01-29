07411c0a0fcbdb453f25492286a7e1cac2b23cab
Piotr <galaspp@rose-hulman.edu>
2021-01-29 10:13:38 -0600

    Began adding Firestore items

--- RoseProject/app/src/main/java/edu/rosehulman/galaspp/roseproject/ui/createeditteam/NavDrawerAdapter.kt
+++ RoseProject/app/src/main/java/edu/rosehulman/galaspp/roseproject/ui/createeditteam/NavDrawerAdapter.kt
@@ -8,6 +8,10 @@ import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
 import edu.rosehulman.galaspp.roseproject.R
 import edu.rosehulman.galaspp.roseproject.ui.project.ProjectObject
 import kotlinx.android.synthetic.main.create_project_modal.view.*
@@ -15,6 +19,41 @@ import kotlinx.android.synthetic.main.create_project_modal.view.*
 class NavDrawerAdapter (var context: Context, var listener: OnNavDrawerListener) : RecyclerView.Adapter<NavDrawerHolder>() {
     private var allTeams : ArrayList<TeamObject> = ArrayList()
     var projects = HashMap<String, ArrayList<ProjectObject>>()
+
+    private val teamRef = FirebaseFirestore
+        .getInstance()
+        .collection("teams")
+
+    init {
+        teamRef
+            .addSnapshotListener { snapshot: QuerySnapshot?, exception: FirebaseFirestoreException? ->
+                if(exception != null) {
+                    Log.e("NAVDRAWERERROR", "Listen Error: $exception")
+                    return@addSnapshotListener
+                }
+                for(teamChange in snapshot!!.documentChanges) {
+                    val team = TeamObject.fromSnapshot(teamChange.document)
+                    when(teamChange.type) {
+                        DocumentChange.Type.ADDED -> {
+//                            Log.d("NAVDRAWERERROR", "Team: $team")
+                            allTeams.add(0, team)
+                            projects[allTeams[0].teamName] = ArrayList()
+                            notifyItemInserted(0)
+                        }
+                        DocumentChange.Type.REMOVED -> {
+                            val pos = allTeams.indexOfFirst { team.id == it.id }
+                            //TODO: Finish this
+                        }
+                        DocumentChange.Type.MODIFIED -> {
+                            //TODO: Finish this
+                            val pos = allTeams.indexOfFirst{ team.id == it.id}
+                            allTeams[pos] = team
+                            notifyItemChanged(pos)
+                        }
+                    }
+                }
+            }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NavDrawerHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.drawer_card_view, parent, false)
@@ -32,7 +71,9 @@ class NavDrawerAdapter (var context: Context, var listener: OnNavDrawerListener)
 
     fun addTeam(newTeam: TeamObject)
     {
-        allTeams.add(0, newTeam)
+//        allTeams.add(0, newTeam)
+        newTeam.projects = ArrayList()
+        teamRef.add(newTeam)
         projects[allTeams[0].teamName] = ArrayList()
         notifyItemInserted(0)
     }
@@ -57,8 +98,10 @@ class NavDrawerAdapter (var context: Context, var listener: OnNavDrawerListener)
         allTeams[position].teamDescription = teamDescription
         allTeams[position].members = members
         allTeams[position].projects = projects
+        this.projects[allTeams[position].teamName] = projects
 
-        notifyItemChanged(position)
+        teamRef.document(allTeams[position].id).set(allTeams[position])
+//        notifyItemChanged(position)
     }
 
     fun showCreateProjectModal(position: Int)
@@ -75,7 +118,11 @@ class NavDrawerAdapter (var context: Context, var listener: OnNavDrawerListener)
             val projectlist = projects[allTeams[position].teamName]
             projectlist?.add(ProjectObject(view.edit_text_project_name.text.toString(),
                     view.edit_text_project_description.text.toString()))
-            notifyItemChanged(position)
+
+            if (projectlist != null) {
+                editTeamAtPosition(position, allTeams[position].teamName,  allTeams[position].teamDescription,  allTeams[position].members, projectlist)
+            }
+//            notifyItemChanged(position)
         }
         builder.setNegativeButton(android.R.string.cancel, null)
         builder.create().show()
--- RoseProject/app/src/main/java/edu/rosehulman/galaspp/roseproject/ui/createeditteam/TeamObject.kt
+++ RoseProject/app/src/main/java/edu/rosehulman/galaspp/roseproject/ui/createeditteam/TeamObject.kt
@@ -1,5 +1,10 @@
 package edu.rosehulman.galaspp.roseproject.ui.createeditteam
 
+import android.util.Log
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.ServerTimestamp
 import edu.rosehulman.galaspp.roseproject.ui.project.ProjectObject
 
 data class TeamObject(var teamName: String = "",
@@ -7,4 +12,17 @@ data class TeamObject(var teamName: String = "",
                       var members: ArrayList<MemberObject> = ArrayList<MemberObject>(),
                       var projects: ArrayList<ProjectObject> = ArrayList()
 ) {
+    @get: Exclude
+    var id = ""
+    @ServerTimestamp
+    var lastTouched: Timestamp? = null
+    companion object {
+        const val LAST_TOUCHED_KEY = "lastTouched"
+        fun fromSnapshot(snapshot: DocumentSnapshot): TeamObject {
+            Log.d("TEST", "${snapshot["teamName"]}")
+            val team = snapshot.toObject(TeamObject::class.java)
+            team!!.id = snapshot.id
+            return team
+        }
+    }
 }
